a9bfb125237c73441c8ccebb4036a48a7b661fa6
Al-amin Yususf <alaminyusuf131@gmail.com>
2020-05-18 23:50:27 +0100

    made changes to models

--- routes/post.js
+++ routes/post.js
@@ -1,35 +1,35 @@
 // REQUIRE EXPRESS
-const express = require('express');
+const express = require('express')
 // Initializing ROUTER
-const router = express.Router();
+const router = express.Router()
 // defining Routes
-const Post = require('../models/Posts');
+const Post = require('../models/Posts')
 // SET ROUTER
 router.get('/', (req, res) => {
-  res.send('we are on post');
-});
+  res.send('we are on post')
+})
 
 router.post('/', async (req, res) => {
   const post = new Post({
     name: req.body.name,
-    email: req.body.email
-  });
+    email: req.body.email,
+  })
 
   try {
     // const saved = await post.save();
-    res.json(post);
-    res.json(req.body).status(200);
+    res.json(post)
+    res.json(req.body).status(200)
   } catch (error) {
-    console.error(error);
+    console.error(error)
   }
-});
+})
 router.get('/posts/:postId', async (req, res) => {
   try {
-    const post = await Post.findById(req.params.postId);
-    res.send(post);
+    const post = await Post.findById(req.params.postId)
+    res.send(post)
   } catch (error) {
-    res.json({ message: error });
+    res.send({ message: error })
   }
-});
+})
 
-module.exports = router;
+module.exports = router
